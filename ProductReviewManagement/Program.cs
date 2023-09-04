8e9e547757c8c8fdca44c85125061fa34de0b779
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-09-04 17:04:55 +0530

    [Nithin]Add: Updation of product Review Management

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.VisualBasic;
+using ProductReviewManagement;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product()
+            {
+                ProductID = 1,
+                UserID = 1,
+                Rating = 1,
+                Review ="BAD",
+                IsLike = false,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 2,
+                UserID = 2,
+                Rating = 2,
+                Review = "NeedTOImprove",
+                IsLike = false,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 3,
+                UserID = 3,
+                Rating = 3,
+                Review = "Average",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 4,
+                UserID = 4,
+                Rating = 4,
+                Review = "Good",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 5,
+                UserID = 5,
+                Rating = 5,
+                Review = "VGood",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 6,
+                UserID = 6,
+                Rating = 5,
+                Review = "VGood",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 7,
+                UserID = 7,
+                Rating = 3,
+                Review = "Average",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 8,
+                UserID = 8,
+                Rating = 4,
+                Review = "Good",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 9,
+                UserID = 9,
+                Rating = 1,
+                Review = "BAD",
+                IsLike = false,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 10,
+                UserID = 10,
+                Rating = 1,
+                Review = "BAD",
+                IsLike = false,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 11,
+                UserID = 11,
+                Rating = 4,
+                Review = "Good",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 12,
+                UserID = 12,
+                Rating = 4,
+                Review = "Good",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 13,
+                UserID = 13,
+                Rating = 5,
+                Review = "VGood",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 14,
+                UserID = 14,
+                Rating = 3,
+                Review = "Average",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 15,
+                UserID = 15,
+                Rating = 1,
+                Review = "BAD",
+                IsLike = false,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 16,
+                UserID = 10,
+                Rating = 3,
+                Review = "Average",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 17,
+                UserID = 10,
+                Rating = 2,
+                Review = "NeedTOImprove",
+                IsLike = false,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 18,
+                UserID = 10,
+                Rating = 2,
+                Review = "NeedTOImprove",
+                IsLike = false,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 19,
+                UserID = 10,
+                Rating = 3,
+                Review = "Average",
+                IsLike = true,
+            });
+            list.Add(new Product()
+            {
+                ProductID = 20,
+                UserID = 10,
+                Rating = 5,
+                Review = "VGood",
+                IsLike = true,
+            });
+
+            Operations operations = new Operations();
+            Console.WriteLine("RetriveTopRecords");
+            operations.RetriveTopRecords(list);
+
+            Console.WriteLine("\nUsingGroupBy");
+            operations.UsingGroupBy(list);
+
+            Console.WriteLine("\nRetriveProductIdAndReview");
+            operations.RetriveProductIdAndReview(list);
+
+            Console.WriteLine("\nSkipTopRecords");
+            operations.SkipTopRecords(list);
+
+            Console.WriteLine("\nAddDataToDataTabel");
+            operations.AddDataToDataTabel(list);
+
+            Console.WriteLine("\nRetriveRecordsFromDataTable");
+            operations.RetriveRecordsFromDataTable();
+
+            Console.WriteLine("\nAvgRating");
+            operations.AvgRating(list);
+
+            Console.WriteLine("\nRetriveUsingReview");
+            operations.RetriveUsingReview(list);
+
+            Console.WriteLine("\nRetriveUsingRating");
+            operations.RetriveUsingRating(list);
+
+            operations.Display(list);
+            
+
+            
+
+        }
+    }
+
+    
+}
+
